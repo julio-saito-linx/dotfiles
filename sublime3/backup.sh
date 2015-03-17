6d327d7bb38c80229857785108fc86054eca1338
Julio Makdisse Saito <saitodisse@gmail.com>
2015-03-17 17:43:28 -0300

    log snnippet

--- sublime3/backup.sh
+++ sublime3/backup.sh
@@ -1,2 +1,2 @@
 echo "copying Sublime Text 3 settings to .dotfiles folder..."
-cp -R /home/`whoami`/.config/sublime-text-3/Packages/User ~/_git/dotfiles/sublime3
+cp -R /home/`whoami`/.config/sublime-text-3/Packages/User .
--- sublime3/install.sh
+++ sublime3/install.sh
@@ -1,4 +1,4 @@
 echo "Updating Sublime Text 3 settings..."
-cp -R ~/_git/dotfiles/sublime3/User /home/`whoami`/.config/sublime-text-3/Packages
+cp -R User /home/`whoami`/.config/sublime-text-3/Packages
 echo "ok!"
 echo
