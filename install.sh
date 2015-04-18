bbace44768349b3856655b2266a35acc013b0f49
Julio Makdisse Saito <saitodisse@gmail.com>
2015-04-17 22:53:00 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
+
+# general configurations
+
 rm -rf "$HOME/.zshrc"
 ln -s "$PWD/.zshrc" "$HOME/.zshrc"
-
-rm -rf "$HOME/.vimrc"
-ln -s "$PWD/.vimrc" "$HOME/.vimrc"
-
-rm -rf "$HOME/.vim"
-ln -s "$PWD/.vim" "$HOME/.vim"
 
 rm -rf "$HOME/.gitignore"
 ln -s "$PWD/gitignore" "$HOME/.gitignore"
@@ -15,8 +12,29 @@ ln -s "$PWD/gitignore" "$HOME/.gitignore"
 rm -rf "$HOME/.gitconfig"
 ln -s "$PWD/.gitconfig" "$HOME/.gitconfig"
 
-rm -rf "$HOME/.atom"
-ln -s "$PWD/.atom" "$HOME/.atom"
+
+# vim configs
+
+rm -rf "$HOME/.vimrc"
+ln -s "$PWD/.vimrc" "$HOME/.vimrc"
+
+rm -rf "$HOME/.vim"
+ln -s "$PWD/.vim" "$HOME/.vim"
+
+
+# atom configs
+
+rm -rf "$HOME/.atom/config.cson"
+ln -s "$PWD/.atom/config.cson" "$HOME/.atom/config.cson"
+
+rm -rf "$HOME/.atom/keymap.cson"
+ln -s "$PWD/.atom/keymap.cson" "$HOME/.atom/keymap.cson"
+
+rm -rf "$HOME/.atom/snippets.cson"
+ln -s "$PWD/.atom/snippets.cson" "$HOME/.atom/snippets.cson"
+
+
+# oh-my-zsh/plugins
 
 rm -rf "$HOME/.oh-my-zsh/plugins/rbates"
 ln -s "$PWD/.oh-my-zsh/plugins/rbates" "$HOME/.oh-my-zsh/plugins/rbates"
