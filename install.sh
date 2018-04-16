f4338d3e1040fcc7bc23c19493511bb0c15c21d9
Julio Makdisse Saito <saitodisse@gmail.com>
2018-04-15 23:12:40 -0300

    lein

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # general configurations
 
 rm -rf "$HOME/.zshrc"
@@ -21,8 +20,8 @@ ln -s "$PWD/gitignore" "$HOME/.gitignore"
 rm -rf "$HOME/.npmignore"
 ln -s "$PWD/.npmignore" "$HOME/.npmignore"
 
-rm -rf "$HOME/.WebStorm2016.2"
-ln -s "$PWD/.WebStorm2016.2" "$HOME/.WebStorm2016.2"
+# rm -rf "$HOME/.WebStorm2016.2"
+# ln -s "$PWD/.WebStorm2016.2" "$HOME/.WebStorm2016.2"
 
 rm -rf "$HOME/.gitconfig"
 ln -s "$PWD/.gitconfig" "$HOME/.gitconfig"
@@ -30,39 +29,39 @@ ln -s "$PWD/.gitconfig" "$HOME/.gitconfig"
 rm -rf "$HOME/.yaourtrc"
 ln -s "$PWD/.yaourtrc" "$HOME/.yaourtrc"
 
-rm -rf "$HOME/.config/sublime-text-3/Packages/User"
-ln -s "$PWD/sublime3-user" "$HOME/.config/sublime-text-3/Packages/User"
+# rm -rf "$HOME/.config/sublime-text-3/Packages/User"
+# ln -s "$PWD/sublime3-user" "$HOME/.config/sublime-text-3/Packages/User"
 
 rm -rf "$HOME/.config/xfce4/xfconf/xfce-perchannel-xml"
 ln -s "$PWD/xfce-perchannel-xml" "$HOME/.config/xfce4/xfconf/xfce-perchannel-xml"
 
 # vim configs
 
-rm -rf "$HOME/.vimrc"
-ln -s "$PWD/.vimrc" "$HOME/.vimrc"
+# rm -rf "$HOME/.vimrc"
+# ln -s "$PWD/.vimrc" "$HOME/.vimrc"
 
-rm -rf "$HOME/.vim"
-ln -s "$PWD/.vim" "$HOME/.vim"
+# rm -rf "$HOME/.vim"
+# ln -s "$PWD/.vim" "$HOME/.vim"
 
 
 # sublime user folder
 
-rm -rf "$HOME/.config/sublime-text-3/Packages/User"
-ln -s "$PWD/sublime3-user" "$HOME/.config/sublime-text-3/Packages/User"
+# rm -rf "$HOME/.config/sublime-text-3/Packages/User"
+# ln -s "$PWD/sublime3-user" "$HOME/.config/sublime-text-3/Packages/User"
 
 # atom configs
 
-rm -rf "$HOME/.atom/config.cson"
-ln -s "$PWD/.atom/config.cson" "$HOME/.atom/config.cson"
+# rm -rf "$HOME/.atom/config.cson"
+# ln -s "$PWD/.atom/config.cson" "$HOME/.atom/config.cson"
 
-rm -rf "$HOME/.atom/keymap.cson"
-ln -s "$PWD/.atom/keymap.cson" "$HOME/.atom/keymap.cson"
+# rm -rf "$HOME/.atom/keymap.cson"
+# ln -s "$PWD/.atom/keymap.cson" "$HOME/.atom/keymap.cson"
 
-rm -rf "$HOME/.atom/snippets.cson"
-ln -s "$PWD/.atom/snippets.cson" "$HOME/.atom/snippets.cson"
+# rm -rf "$HOME/.atom/snippets.cson"
+# ln -s "$PWD/.atom/snippets.cson" "$HOME/.atom/snippets.cson"
 
 
 # oh-my-zsh/plugins
 
-rm -rf "$HOME/.oh-my-zsh/custom/plugins/azk"
-ln -s "$PWD/.oh-my-zsh/custom/plugins/azk" "$HOME/.oh-my-zsh/custom/plugins/azk"
+# rm -rf "$HOME/.oh-my-zsh/custom/plugins/azk"
+# ln -s "$PWD/.oh-my-zsh/custom/plugins/azk" "$HOME/.oh-my-zsh/custom/plugins/azk"
