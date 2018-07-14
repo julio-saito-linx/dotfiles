fb76c2d8d590c2ae864c7f839143dce3dc7ec798
Julio Makdisse Saito <saitodisse@gmail.com>
2018-07-13 22:14:16 -0300

    conf xfce

--- bin/take-screenshot.sh
+++ bin/take-screenshot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 now=$(date +"%F_%H-%M-%S")
-ffmpeg -f x11grab -video_size 1400x900 -i $DISPLAY -vframes 1 ~/Imagens/screenshots/$now.png
+ffmpeg -f x11grab -video_size 1920x1080 -i $DISPLAY -vframes 1 ~/Imagens/screenshots/$now.png
